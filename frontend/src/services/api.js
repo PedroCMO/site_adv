5bd0a2e91647082c4506992212f493dfc67b6268
PedroCMO <pedrochristo17@gmail.com>
2026-06-11 13:50:13 -0300

    Adiciona configs do front

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 // frontend/src/services/api.js
 
-const BASE_URL = 'http://127.0.0.1:8000/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
 
 export const fetchAdvogados = async () => {
   const resposta = await fetch(`${BASE_URL}/advogados/`);
