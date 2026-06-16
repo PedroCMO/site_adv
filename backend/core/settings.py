16aac8acff5f6a1db981c3068225a18801484d2c
PedroCMO <pedrochristo17@gmail.com>
2026-06-16 18:03:49 -0300

    Atualiza STORAGES para usar Cloudinary no Django 6.0

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -138,4 +138,11 @@ CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
